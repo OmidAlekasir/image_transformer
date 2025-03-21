8b73617ee727be4bbe98ff4bb5f3500ddb8a3ee9
OmidAlekasir <majid.alekasir@gmail.com>
2025-03-21 15:45:25 -0700

    library structure almost complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-from spherical_homography import ImageTransformer
+from spherical_transformer import SphericalTransformer
 
 if __name__ == '__main__':
     
@@ -8,11 +8,22 @@ if __name__ == '__main__':
 
     height, width, _ = src.shape
 
-    transformer = ImageTransformer(width, height, 70)
-    H = transformer.transform(alpha = 0, beta = 0, gamma = 0)
-    dst = cv2.warpPerspective(src, H, (width, height))
+    T = SphericalTransformer(width, height, 70)
+    
+    # H = transformer.transform(alpha = 0, beta = 0, gamma = 0)
+    # dst = cv2.warpPerspective(src, H, (width, height))
 
-    cv2.imshow('src', src)
-    cv2.imshow('dst', dst)
+    beta = 0
 
-    cv2.waitKey(10000000)
+    while 1:
+        beta += 1
+
+        T.rotate(beta = beta)
+
+        H = T.get_homography()
+        dst = cv2.warpPerspective(src, H, (width, height))
+        
+        cv2.imshow('dst', dst)
+
+        if cv2.waitKey(10) == 'q':
+            break
